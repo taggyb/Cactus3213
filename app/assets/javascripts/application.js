2e83dec7d54689c34f83f456fa57e53a51e81854
endlessfrank <endlessfrank@qq.com>
2013-09-15 21:12:56 +0800

    Add movie

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery_ujs
+//= require jquery.form.js
 //= require twitter/bootstrap
 //= require underscore
 //= require backbone
--- app/assets/javascripts/views/movies/newMovie.js
+++ app/assets/javascripts/views/movies/newMovie.js
@@ -1,41 +1,41 @@
 Cactus2.Views.NewMovie = Backbone.View.extend({
-	template: JST["movies/newMovie"],
-
-	render: function() {
-		this.$el.html(this.template());
- 		return this;
-	},
-
-	events: {
-		"click #create"	: "saveMovie",
-		"click #cancel"	: "cancelMovie"
-	},
-
-	saveMovie: function(e) {
-		e.preventDefault();
-		if (typeof gon == 'undefined'){
-			alert('Please login!');
-		} 
-		
-		
-		$('#new_movie_form').ajaxSubmit({
-			method: "POST",
-			url: "http://cs3213.herokuapp.com/movies.json",
-			data: {
-				"access_token": gon.token
-			},
-			dataType: "json",
-			success: function(e){
-				alert("movie added");
-				window.routerHome.navigate('/', {trigger: true});
-			},
-			error: function(e) {
-					alert("movie not added!");
-				}
-			});
-	},
-
-	cancelMovie: function() {
-		routerHome.navigate("", { trigger: true });
-	}
+        template: JST["movies/newMovie"],
+ 
+        render: function() {
+                this.$el.html(this.template());
+                return this;
+        },
+ 
+        events: {
+                "click #create" : "saveMovie",
+                "click #cancel" : "cancelMovie"
+        },
+ 
+        saveMovie: function(e) {
+                e.preventDefault();
+                if (typeof gon == 'undefined'){
+                        alert('Please login!');
+ 
+                        return false;
+                }
+               
+                $(e.target).closest('form').ajaxSubmit({
+                        url: 'http://cs3213.herokuapp.com/movies.json',
+                        dataType:'json',
+                        data: {        
+                                access_token: gon.token
+                        },
+                        method: "POST",
+                        error: function(e){
+                                alert("error");
+                        },
+                        success: function(e){
+                                alert("movie added."); 
+                        },
+                });
+        },
+ 
+        cancelMovie: function() {
+                routerHome.navigate("", { trigger: true });
+        }
 });
