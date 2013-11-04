164200d03502fa641dc4e878f7d3fcc54dab0eb4
Zhao Yue <zhaoyue@ubuntu.(none)>
2013-11-04 15:36:57 +0800

    fixed multiple event binding bug

--- app/assets/javascripts/models/movie.js
+++ app/assets/javascripts/models/movie.js
@@ -8,6 +8,11 @@ Cactus2.Models.Movie=Backbone.Model.extend({
 		img_url: null,
 		user: null,
 	},
+
+	initialize: function(options) {
+		this.id = options.id || null;   
+	},
+
 	url: function() {
         return 'http://cs3213.herokuapp.com/movies/'+this.id+'.json';
     }
